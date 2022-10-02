ba69bf3ae082c80ceccb711e973d5f6177207d01
Jumian <60074578+Jumian@users.noreply.github.com>
2022-10-02 14:29:47 +0200

    Added input with prompt

--- Skillbox_cpp_23_3/main.cpp
+++ Skillbox_cpp_23_3/main.cpp
@@ -7,20 +7,25 @@
 bool underloaded(int number){return (number < OPT? true: false);}
 bool overloaded(int number) {return (number>OPT?true:false);}
 
-
-#define PRINT(STRING) std::cout <<"Carriege #"<< (STRING)<< " is ";
+#define PRINT(STRING) std::cout << STRING ;
 #define PRINTLN(STRING) std::cout << (STRING) << std::endl;
+#define PRINTCAR(STRING) PRINT("Carriege #") PRINT(STRING) PRINT(" is ");
+#define PRINTPROMPT PRINT("Please input ") PRINT(CARIIEGES_NUM)  \
+PRINT(" numbers of people on carieges from 0 to ") PRINT(CARIIEGES_NUM) PRINTLN(":")
 #define OVERLOADED(NUM) overloaded(NUM)
 #define UNDERLOADED(NUM) underloaded(NUM)
 #define CHK(CASE) if(CASE)
-#define CHK_OVERLOAD(_num,__car) CHK(OVERLOADED(_num)) {PRINT(__car) PRINTLN("Overloaded") }
-#define CHK_UNDERLOAD(_num,__car) CHK(UNDERLOADED(_num)) {PRINT(__car) PRINTLN("Underloaded") }
+#define CHK_OVERLOAD(_num,__car) CHK(OVERLOADED(_num)) {PRINTCAR(__car) PRINTLN("Overloaded") }
+#define CHK_UNDERLOAD(_num,__car) CHK(UNDERLOADED(_num)) {PRINTCAR(__car) PRINTLN("Underloaded") }
 #define CHK_CAR_OVER(CAR,N)  CHK_OVERLOAD(CAR[N],N)
 #define CHK_CAR_UNDER(CAR,N) CHK_UNDERLOAD(CAR[N],N)
-#define FORLOOP(__var,__n,__arr,__CHK) for(int __var=0;__var<__n;++__var) { __CHK(__arr,__var) }
+#define INPUT(__arr,__index) std::cin >> __arr[__index];
+#define FORLOOP(__index,__n,__arr,__ACT) for(int __index=0;__index<__n;++__index) { __ACT(__arr,__index) }
 
 int main() {
     int ARR_VAR[CARIIEGES_NUM];
+    PRINTPROMPT
+    FORLOOP(i,CARIIEGES_NUM,ARR_VAR, INPUT)
     FORLOOP(i,CARIIEGES_NUM,ARR_VAR, CHK_CAR_OVER)
     FORLOOP(i,CARIIEGES_NUM,ARR_VAR, CHK_CAR_UNDER)
 }
